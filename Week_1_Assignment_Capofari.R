7fe7a6db14934801fc76647e18432e2ab86e6886
Nicholas Capofari <ncapofari@gmail.com>
2015-07-06 15:25:53 -0400

    Add Week_1_Assignment_Capofari.R

---
+++ Week_1_Assignment_Capofari.R
@@ -0,0 +1,16 @@
+#12!
+c <- 1
+for(i in 2:12){
+	c <- c*i
+	}
+print(c)
+
+#numeric vector
+seq(from = 20, to = 50, by = 5)
+
+#a,b,c inputs for quadratic equation
+a <- 1
+b <- 2
+c <- 3
+q <- matrix(c(1,2,3), ncol=1)
+polyroot(q)
